e38120665a1eb11faefdbeed008656fe4c9c20d9
muyuchengfenglin <745544921@qq.com>
2017-12-06 03:26:00 +0800

    ...

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
 require('vendor/autoload.php');
-$config = new Muyu\Config();
-$config->tryInit(['b' => 2]);
-$config->dump();
+$curl = new Muyu\Curl('https://moodrain.cn/api/method');
+echo $curl->get();
--- src/Curl.php
+++ src/Curl.php
@@ -3,6 +3,210 @@ namespace Muyu;
 
 class Curl
 {
+    private $curl;
+    private $url;
+    private $query;
+    private $mothod;
+    private $responseType;
+    private $data;
+    private $file;
+    private $header;
+    private $result;
+
+    public function __construct(String $url)
+    {
+        $this->url = $url;
+        $this->curl = curl_init();
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($this->curl, CURLOPT_SSL_VERIFYPEER, 0);
+    }
+    public function url(String $url)
+    {
+        $this->url = $url;
+        return $this;
+    }
+    public function fullUrl(String $url = null)
+    {
+        if($url)
+        {
+            $this->url = $url;
+            return $this;
+        }
+        else
+            return $this->url . ( $this->query ? '?' . http_build_query($this->query) : '');
+    }
+    public function query(Array $query)
+    {
+        $this->query = $query;
+        return $this;
+    }
+    public function receive(String $responseType)
+    {
+        if(!in_array($responseType, ['text', 'json', 'xml']))
+            throw new \Exception ('unsupported responseType');
+        $this->responseType = $responseType;
+        return $this;
+    }
+    public function stringData(String $data)
+    {
+        $this->data = $data;
+        return $this;
+    }
+    public function data(Array $data)
+    {
+        $this->data = http_build_query($data);
+        return $this;
+    }
+    public function file(String $file)
+    {
+        $this->file = $file;
+        return $this;
+    }
+    public function cookie(Array $cookie = null)
+    {
+        if($cookie)
+        {
+            $cookieStr = '';
+            foreach($cookie as $key => $val)
+                $cookieStr .= $key . '=' . $val . ';';
+            curl_setopt($this->curl, CURLOPT_COOKIE, $cookieStr);
+            return $this;
+        }
+        else
+        {
+            $cookie = [];
+            foreach($this->result['header']['Set-Cookie'] as $info)
+            {
+                $info = explode(';', $info)[0];
+                $key = explode('=', $info)[0];
+                $val = explode('=', $info)[1];
+                $cookie[$key] = $val;
+            }
+            return $cookie;
+        }
+    }
+    public function header(Array $header = null)
+    {
+        if($header)
+        {
+            $headerData = [];
+            foreach($header as $key => $value)
+                array_push($headerData, $key . ': ' . $value);
+            curl_setopt($this->curl, CURLOPT_HTTPHEADER, $headerData);
+            return $this;
+        }
+        else
+            return $this->result['header'];
+    }
+    public function content()
+    {
+        return $this->result['content'];
+    }
+
+    
+    public function get()
+    {
+        curl_setopt($this->curl, CURLOPT_URL, $this->fullUrl());
+        return $this->format($this->handle($this->curl));
+    }
+    public function post()
+    {
+        curl_setopt($this->curl, CURLOPT_URL, $this->fullUrl());
+        curl_setopt($this->curl, CURLOPT_POST, 1);
+        if($this->data)
+            curl_setopt($this->curl, CURLOPT_POSTFIELDS, $this->data);
+        if($this->file)
+        {
+            $stream = fopen($this->file, 'r');
+            $size = filesize($this->file);
+            curl_setopt($this->curl, CURLOPT_INFILE, $stream);
+            curl_setopt($this->curl, CURLOPT_INFILESIZE, $size);
+        }
+        return $this->format($this->handle($this->curl));
+    }
+    public function put()
+    {
+        curl_setopt($this->curl, CURLOPT_URL, $this->fullUrl());
+        curl_setopt($this->curl, CURLOPT_PUT, 1);
+        if($this->file)
+        {
+            $stream = fopen($this->file, 'r');
+            $size = filesize($this->file);
+            curl_setopt($this->curl, CURLOPT_INFILE, $stream);
+            curl_setopt($this->curl, CURLOPT_INFILESIZE, $size);
+        }
+        if($this->data)
+            curl_setopt($this->curl, CURLOPT_POSTFIELDS, $this->data);
+        return $this->format($this->handle($this->curl));
+    }
+    public function delete()
+    {
+        curl_setopt($this->curl, CURLOPT_URL, $this->fullUrl());
+        curl_setopt($this->curl, CURLOPT_CUSTOMREQUEST, 'DELETE');
+        return $this->format($this->handle($this->curl));
+    }
+    public function patch()
+    {
+        curl_setopt($this->curl, CURLOPT_URL, $this->fullUrl());
+        curl_setopt($this->curl, CURLOPT_CUSTOMREQUEST, 'PATCH');
+        return $this->format($this->handle($this->curl));
+    }
+
+    private function format(String $raw)
+    {
+        switch ($this->responseType)
+        {
+            case 'json': return json_decode($response, true);
+            case 'xml' : return Muyu\XML::parse($response);
+            case 'jpg' : header('content-type: image/jpeg');return $response;
+            case 'png' : header('content-type: image/png');return $response;
+            case 'gif' : header('content-type: image/gif');return $response;
+            case 'bmp' : header('content-type: image/bmp');return $response;
+            default    : return $response;
+        }
+    }
+    private function handle($curl)
+    {
+        curl_setopt($curl, CURLOPT_HEADER, 1);
+        $content = curl_exec($curl);
+        $response = explode("\r\n", $content);
+        $headers = [];
+        $headers['Set-Cookie'] = [];
+        $content = '';
+        $endHeader = false;
+        $count = 0;
+        foreach($response as $row)
+        {
+            $count++;
+            if(!$endHeader)
+            {
+                $header = explode(': ', $row);
+                if(count($header) == 1 && strlen($row) != 0)
+                {
+                    $headers['Status'] = $header[0];
+                    continue;
+                }
+                if(strlen($row))
+                {
+                    if($header[0] == 'Set-Cookie')
+                        $headers[$header[0]][] = $header[1];
+                    else
+                        $headers[$header[0]] = $header[1];
+                }
+                else
+                    $endHeader = true;
+            }
+            else
+                $content .= $row . ($count == count($response) ? '' : "\r\n");
+        }
+        $this->result['header'] = $headers;
+        $this->result['content'] = $content;
+        return $this->content();
+    }
+    public function close()
+    {
+        curl_close($this->curl);
+    }
     public function do($url, $type=null, $res=null, $data=null, $header=null, $useCookie=null, $cookie=null)
     {
         $curl = curl_init();
